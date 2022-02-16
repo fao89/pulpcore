7b27f2571a87283df7c2004cceb4f9b1795c802a
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2022-02-16 12:09:59 +0000

    Fix black issues
    [noissue]

--- pulpcore/app/management/commands/handle-artifact-checksums.py
+++ pulpcore/app/management/commands/handle-artifact-checksums.py
@@ -76,7 +76,7 @@ class Command(BaseCommand):
         if content.count():
             self.stdout.write(
                 _("There is approx {:.2f}Mb of content to be downloaded.").format(
-                    ras_size / (1024 ** 2)
+                    ras_size / (1024**2)
                 )
             )
 
@@ -111,7 +111,7 @@ class Command(BaseCommand):
         if content.count() > 0:
             self.stdout.write(
                 _("There is approx. {:.2f}Mb content data to be re-hashed.").format(
-                    artifacts.aggregate(Sum("size"))["size__sum"] / (1024 ** 2)
+                    artifacts.aggregate(Sum("size"))["size__sum"] / (1024**2)
                 )
             )
 
